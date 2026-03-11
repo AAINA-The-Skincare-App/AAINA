31b39c6392ab8159e266aaa80bd6dfb22dff0a2d
pmanocha07 <pmanocha553@gmail.com>
2026-03-11 10:02:42 +0530

    Initial project setup

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AAINA
+//
+//  Created by GEU on 10/03/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
