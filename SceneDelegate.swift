267541bfba445532a8517888446e1459d00553c5
Neha <neha.uk06@gmail.com>
2026-03-11 11:58:32 +0530

    Setup DataModel and configure UIWindow in SceneDelegate
    Initialize DataModel and set up root view controller in scene.

--- SceneDelegate.swift
+++ SceneDelegate.swift
@@ -1,8 +1,8 @@
 //
 //  SceneDelegate.swift
-//  AAINA
+//  skinCare
 //
-//  Created by GEU on 10/03/26.
+//  Created by GEU on 14/02/26.
 //
 
 import UIKit
@@ -10,13 +10,31 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    var dataModel: DataModel!
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        //guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = scene as? UIWindowScene else { return }
+
+                dataModel = DataModel()
+
+                let window = UIWindow(windowScene: windowScene)
+                self.window = window
+
+                let storyboard = UIStoryboard(name: "Main", bundle: nil)
+
+                guard let nav = storyboard.instantiateInitialViewController() as? UINavigationController else { return }
+
+                if let dobVC = nav.viewControllers.first as? OnboardingDOBViewController {
+                    dobVC.dataModel = dataModel
+                }
+
+                window.rootViewController = nav
+                window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
