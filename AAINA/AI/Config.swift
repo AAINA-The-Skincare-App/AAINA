fc9ec5124386d0bdb23ddf65ff199292987ebbf3
pmanocha07 <pmanocha553@gmail.com>
2026-04-29 13:25:45 +0530

    fixed errors

--- AAINA/AI/Config.swift
+++ AAINA/AI/Config.swift
@@ -2,6 +2,6 @@
 // Create a Config.example.swift with empty apiKey for teammates
 
 enum Config {
-    static let geminiAPIKey = "" // ⚠️Get a new key from Google AI Studio and paste it here (do NOT commit it)
+    static let geminiAPIKey = "AIzaSyDI2rqUtIRulELgfnQP3Ei2pfEchnuX358" // ⚠️Get a new key from Google AI Studio and paste it here (do NOT commit it)
     static let geminiEndpoint = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent"
 }
